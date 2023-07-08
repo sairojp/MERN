a51bdefe5e2e7e66b29206f78d9231c4ae0fa197
N0t0p <sairoj25@gmail.com>
2023-07-08 22:43:18 +0545

    adding homepage react component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
-    <Login/>
+    <Homepage/>
   );
 }
 
