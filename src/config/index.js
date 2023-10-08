59b9a4b643fef00a21e523e4dd36e1739792d544
N0t0p <sairoj25@gmail.com>
2023-10-08 09:29:18 +0545

    refinement

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+export const API_BASE_URL = "http://localhost:5000";
+export const API_VERSION = "/api/v1/";
+export const LOGIN_API = API_BASE_URL + API_VERSION + "login";
+export const USER_API = API_BASE_URL + API_VERSION + "user";
+export const USERS_API = API_BASE_URL + API_VERSION + "users";
+export const POST_API = API_BASE_URL + API_VERSION + "post";
+export const POSTS_API = API_BASE_URL + API_VERSION + "posts";
