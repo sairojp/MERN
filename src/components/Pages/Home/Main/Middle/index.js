31f83293b46fbd57809e28b4db2062fba92e2265
N0t0p <sairoj25@gmail.com>
2023-10-08 09:16:02 +0545

    adding functional component

--- src/components/Pages/Home/Main/Middle/index.js
+++ src/components/Pages/Home/Main/Middle/index.js
@@ -1,19 +1,18 @@
-import { Component } from "react";
+
 import NewPost from "./NewPost";
 import PostList from "./PostList";
 
-class Middle extends Component {
-  render() {
-    const {user} = this.props
-    return (
-      <div className="col-lg-6 col-md-8 no-pd">
-        <div className="main-ws-sec">
-          <NewPost user={user} />
-          <PostList user={user} />
-        </div>
+
+const Middle = (props) => {
+  const { user } = props;
+  return (
+    <div className="col-lg-6 col-md-8 no-pd">
+      <div className="main-ws-sec">
+        <NewPost user={user} />
+        <PostList user={user} searchKey={props.searchKey} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Middle;
